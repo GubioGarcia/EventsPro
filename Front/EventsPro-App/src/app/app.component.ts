d6ee07114d06899982718ec6b831391eddc74690
Gubio Garcia <gubiogarcia@gmail.com>
2024-04-02 15:14:44 -0300

    Feat: request HTTP Angular

--- Front/EventsPro-App/src/app/app.component.ts
+++ Front/EventsPro-App/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EventsComponent } from "./events/events.component";
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, EventsComponent]
+    imports: [RouterOutlet, HttpClientModule, EventsComponent]
 })
 export class AppComponent {
   title = 'EventsPro-App';
--- Front/EventsPro-App/src/app/events/events.component.ts
+++ Front/EventsPro-App/src/app/events/events.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { error } from 'console';
+import { response } from 'express';
 
 @Component({
   selector: 'app-events',
@@ -9,4 +12,18 @@ import { Component } from '@angular/core';
 })
 export class EventsComponent {
 
+  public events: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getEvents();
+  }
+
+  public getEvents(): void {
+    this.http.get('https://localhost:5001/api/events').subscribe(
+      response => this.events = response,
+      error => console.log(error)
+    );
+  }
 }
