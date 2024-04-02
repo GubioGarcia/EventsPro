337fc192f141db35fecfa2905f8d72a054d6fc8a
Gubio Garcia <gubiogarcia@gmail.com>
2024-04-02 14:50:15 -0300

    Fix: front and components

--- Front/EventsPro-App/src/app/app.component.ts
+++ Front/EventsPro-App/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EventsComponent } from "./events/events.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [RouterOutlet, EventsComponent]
 })
 export class AppComponent {
   title = 'EventsPro-App';
