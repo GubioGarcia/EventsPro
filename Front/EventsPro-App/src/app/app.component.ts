de553a842fd3bb58fcd3e4d8909d416c0e8b05c0
Gubio Garcia <gubiogarcia@gmail.com>
2024-04-03 15:28:51 -0300

    Feat: navbar with bootstrap

--- Front/EventsPro-App/src/app/app.component.ts
+++ Front/EventsPro-App/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EventsComponent } from "./events/events.component";
 import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, HttpClientModule, EventsComponent]
+    imports: [RouterOutlet, HttpClientModule, EventsComponent, NavComponent]
 })
 export class AppComponent {
   title = 'EventsPro-App';
